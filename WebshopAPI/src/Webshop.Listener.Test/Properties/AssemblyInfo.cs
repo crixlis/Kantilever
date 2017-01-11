0b2b78c20fbb932f86076063dd90876bc98a3df8
pmeijden <patrickvdmeijden@live.nl>
2017-01-11 16:36:50 +0100

    architectuur finished

---
+++ WebshopAPI/src/Webshop.Listener.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Webshop.Listener.Test")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f6d7e52-e2da-4319-8379-43017ef5e3a9")]
---
+++ WebshopAPI/src/Webshop.Listener.Test/WebshopListenerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webshop.Listener.Test
+{
+    public class WebshopListenerTests
+    {
+        public WebshopListenerTests()
+        {
+        }
+    }
+}
