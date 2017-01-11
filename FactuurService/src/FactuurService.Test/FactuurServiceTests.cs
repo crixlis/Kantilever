eec8ee9f969766d50f5294141612c1758613fa42
pmeijden <patrickvdmeijden@live.nl>
2017-01-11 16:19:57 +0100

    FactuurService

---
+++ FactuurService/src/FactuurService.Test/FactuurServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactuurService.Test
+{
+    public class FactuurServiceTests
+    {
+        public FactuurServiceTests()
+        {
+        }
+    }
+}
