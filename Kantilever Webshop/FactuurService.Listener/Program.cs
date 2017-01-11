61ce79b184e320a8b1aa11be7fc23b7f7cfa16f7
pmeijden <patrickvdmeijden@live.nl>
2017-01-11 14:21:20 +0100

    Listeners naar console app

--- Kantilever Webshop/FactuurService.Listener/Program.cs
+++ Kantilever Webshop/FactuurService.Listener/Program.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BestelService.Listener
+namespace FactuurService.Listener
 {
-    public class Class1
+    public class Program
     {
-        public Class1()
+        public static void Main(string[] args)
         {
         }
     }
