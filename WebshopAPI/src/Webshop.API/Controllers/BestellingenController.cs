56533465507eeff59b6f6ff97adacac31c586383
Gino Bos <GinoB@CURSISTM04>
2017-01-12 13:57:04 +0100

    valuescontroller is nu bestellingencontroller

--- WebshopAPI/src/Webshop.API/Controllers/BestellingenController.cs
+++ WebshopAPI/src/Webshop.API/Controllers/BestellingenController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Webshop.API.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class BestellingenController : Controller
     {
         // GET api/values
         [HttpGet]
