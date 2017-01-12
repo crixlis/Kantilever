7af1d69cc0283fb7753e01bfe37edb821b875d34
Jeroen Duel <JeroenD@CURSISTM07>
2017-01-12 15:40:26 +0100

    nginx draait op poort 80, niet 4200

--- WebshopFrontend/deploy.cmd
+++ WebshopFrontend/deploy.cmd
@@ -1,11 +1,11 @@
 cd particulier
 @echo Build Angular project particulier
-call ng build
+call ng build --prod
 @echo Build succesfull!
 @echo Starting docker image build
 call docker build --tag="kantilever/webshopfrontend:1.1" .
 @echo Image build succesfull!
 @echo Run docker image
-call docker run -d -p 1024:4200 kantilever/webshopfrontend:1.1
+call docker run -d -p 8080:80 -d kantilever/webshopfrontend:1.1
 @echo Docker image running!
 cd ..
