813f91cc5b5a9525f6d5519e0e9d98b416f75ae5
Jeroen Duel <JeroenD@CURSISTM07>
2017-01-12 15:11:55 +0100

    -d added to docker run

--- WebshopFrontend/deploy.cmd
+++ WebshopFrontend/deploy.cmd
@@ -6,6 +6,6 @@ call ng build
 call docker build --tag="kantilever/webshopfrontend:1.1" .
 @echo Image build succesfull!
 @echo Run docker image
-call docker run -p 1024:4200 kantilever/webshopfrontend:1.1
+call docker run -d -p 1024:4200 kantilever/webshopfrontend:1.1
 @echo Docker image running!
 cd ..
