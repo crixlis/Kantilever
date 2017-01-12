20ff5999c62fd0d84ce41eef745003685882de40
pmeijden <patrickvdmeijden@live.nl>
2017-01-12 10:51:25 +0100

    BestelService kan berichtje opvangen en versturen

--- BestelService/src/BestelService.Test/BestelServiceTests.cs
+++ BestelService/src/BestelService.Test/BestelServiceTests.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using NSubstitute;
+using rabbitmq_demo;
+using Xunit;
 
 namespace BestelService.Test
 {
     public class BestelServiceTests
     {
+        [Fact]
+        public void IkWilEenBestellingAanmakenEventOpvangenEnEenBestellingAangemaaktPublishen()
+        {
+            //Arrange
+            var sender = Substitute.For<ISender>();
+            var service = new BestelService(sender);
+
+            var bestelling = new bestellingAanmaken
+            {
+                Id = 1
+            };
+
+            //Act
+            service.Execute(bestelling);
+
+            //Assert
+            sender.Received(1).PublishEvent(Arg.Any<bestellingAangemaakt>());
+        }
     }
 }
---
+++ BestelService/src/BestelService/BestelService.cs
@@ -0,0 +1,23 @@
+﻿using rabbitmq_demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestelService
+{
+    public class BestelService: IReceive<bestellingAanmaken>
+    {
+        ISender _sender;
+
+        public BestelService(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public void Execute(bestellingAanmaken item)
+        {
+            _sender.PublishEvent(new bestellingAangemaakt());
+        }
+    }
+}
