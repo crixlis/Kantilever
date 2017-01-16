60c3bdbc15e80c481bc1ea5786297ebfc5d321d0
Gino Bos <GinoB@CURSISTM04>
2017-01-16 16:10:25 +0100

    winkelwagensamenvatting toegevoegd

---
+++ WebshopFrontend/particulier/src/app/winkelwagen/winkelwagen.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { WinkelwagenComponent } from './winkelwagen.component';
+
+@NgModule({
+  declarations: [
+    WinkelwagenComponent
+  ],
+  imports: [
+  ],
+  providers: []
+})
+export class WinkelwagenModule { }
