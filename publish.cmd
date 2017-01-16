91827b19cc3eb6d19507aeb4501d115faad98a5e
n.knaake@gmail.com <n.knaake@gmail.com>
2017-01-16 13:46:23 +0100

    added set-locations to the publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -1,5 +1,11 @@
+cd .\WebshopBeheer\src\WebshopBeheer
 call dotnet publish -o $(Build.Repository.LocalPath)\WebshopBeheer\src\WebshopBeheer\bin\publish -c release
+cd .\BestelService\src\BestelService
 call dotnet publish -o .\BestelService\src\BestelService\bin\publish -c release
+cd .\BestelService\src\BestelService
 call dotnet publish -o .\WebshopAPI\src\Webshop.API\bin\publish -c release
+cd .\WebshopAPI\src\Webshop.API
 call dotnet publish -o .\WebshopAPI\src\Webshop.Listener\bin\publish -c release
+cd .\WebshopAPI\src\Webshop.Listener
 call dotnet publish -o .\FactuurService\src\FactuurService\bin\publish -c release
+cd .\FactuurService\src\FactuurService
