9e91ee0c5bd8ad29e829ec4b2254da1f4974b8cf
Nick Knaake <>
2017-01-16 14:26:48 +0100

    Updated publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -1,11 +1,11 @@
-cd .\WebshopBeheer\src\WebshopBeheer
-call dotnet publish -o $(Build.Repository.LocalPath)\WebshopBeheer\src\WebshopBeheer\bin\publish -c release
-cd $(Build.Repository.LocalPath)\BestelService\src\BestelService
+cd %Build.Repository.LocalPath%\WebshopBeheer\src\WebshopBeheer
+call dotnet publish -o %Build.Repository.LocalPath%\WebshopBeheer\src\WebshopBeheer\bin\publish -c release
+cd %Build.Repository.LocalPath%\BestelService\src\BestelService
 call dotnet publish -o .\BestelService\src\BestelService\bin\publish -c release
-cd $(Build.Repository.LocalPath)\WebshopAPI\src\Webshop.API
+cd %Build.Repository.LocalPath%\WebshopAPI\src\Webshop.API
 call dotnet publish -o .\WebshopAPI\src\Webshop.API\bin\publish -c release
-cd $(Build.Repository.LocalPath)\WebshopAPI\src\Webshop.Listener
+cd %Build.Repository.LocalPath%\WebshopAPI\src\Webshop.Listener
 call dotnet publish -o .\WebshopAPI\src\Webshop.Listener\bin\publish -c release
-cd $(Build.Repository.LocalPath)\FactuurService\src\FactuurService
+cd %Build.Repository.LocalPath%\FactuurService\src\FactuurService
 call dotnet publish -o .\FactuurService\src\FactuurService\bin\publish -c release
 
