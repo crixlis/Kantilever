9dd1402c03634518da29d9d636f2e7205a4f6112
Gino Bos <GinoB@CURSISTM04>
2017-01-16 17:12:54 +0100

    lelijke styling van Jeroen ;-)

--- WebshopFrontend/particulier/src/app/app.component.ts
+++ WebshopFrontend/particulier/src/app/app.component.ts
@@ -1,43 +1,17 @@
-import {
-  Component,
-  OnChanges,
-  Input,
-  trigger,
-  state,
-  style,
-  transition,
-  animate
-} from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
-    animations: [
-    trigger('togglenavChanged', [
-      state('true' , style({ opacity: 1, transform: 'scale(1.0)' })),
-      state('false', style({ opacity: 0, transform: 'scale(0.0)'  })),
-      transition('1 => 0', animate('300ms')),
-      transition('0 => 1', animate('900ms'))
-    ])
-  ]
-
 })
 
-
-export class AppComponent implements OnChanges  {
-  title = 'Kantilever!';
-
-  @Input() isVisible : boolean = false;
-
-  ngOnChanges() {
-
-  }
+export class AppComponent {
 
   ToggleSideNav() {
     var sidebar = document.querySelector('#sidebar');
-    if(sidebar.className == '')
+
+    if(sidebar.className == '' || !sidebar.className)
     {
       sidebar.className = 'active';
     }
@@ -45,8 +19,6 @@ export class AppComponent implements OnChanges  {
     { 
       sidebar.className = '';
     }
-
-    
   }
   
 }
