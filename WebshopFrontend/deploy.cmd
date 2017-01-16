1b00b0235f8e28b99d74de0228f450d97e13873c
n.knaake@gmail.com <n.knaake@gmail.com>
2017-01-16 10:29:44 +0100

    changed the deploy.cmd of the WebshopFrontend Project

--- WebshopFrontend/deploy.cmd
+++ WebshopFrontend/deploy.cmd
@@ -1,11 +1,4 @@
-cd particulier
-@echo Build Angular project particulier
+call npm install
+@echo Install succesfull!
 call ng build --prod
 @echo Build succesfull!
-@echo Starting docker image build
-call docker build --tag="kantilever/webshopfrontend:1.1" .
-@echo Image build succesfull!
-@echo Run docker image
-call docker run -d -p 8080:80 -d kantilever/webshopfrontend:1.1
-@echo Docker image running!
-cd ..
