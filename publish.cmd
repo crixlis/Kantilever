36170168a98337eeda77a8677184d59d37267c4e
Nick Knaake <>
2017-01-18 09:40:50 +0100

    Updated publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -1,11 +1,11 @@
 cd %1\WebshopBeheer\src\WebshopBeheer
-call dotnet publish -o %1\publish\WebshopBeheer -c release
+call dotnet publish -o %1\WebshopBeheer\src\WebshopBeheer\bin\publish -c release
 cd %1\BestelService\src\BestelService
-call dotnet publish -o %1\publish\BestelService -c release
+call dotnet publish -o %1\BestelService\src\BestelService\bin\publish -c release
 cd %1\WebshopAPI\src\Webshop.API
-call dotnet publish -o %1\publish\Webshop.API -c release
+call dotnet publish -o %1\WebshopAPI\src\Webshop.API\bin\publish -c release
 cd %1\WebshopAPI\src\Webshop.Listener
-call dotnet publish -o %1\publish\Webshop.Listener -c release
+call dotnet publish -o %1\\WebshopAPI\src\Webshop.Listener\bin\publish -c release
 cd %1\FactuurService\src\FactuurService
-call dotnet publish -o %1\publish\FactuurService -c release
+call dotnet publish -o %1\FactuurService\src\FactuurService\bin\publish -c release
 
