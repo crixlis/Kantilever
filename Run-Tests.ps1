d29be73fa0ba900f557f2a9e118e88e4bee041aa
n.knaake@gmail.com <n.knaake@gmail.com>
2017-01-18 11:59:57 +0100

    .

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -5,7 +5,7 @@ param(
 foreach ($folder in $(Get-ChildItem -Path $rootURL | Where-Object{ $_.PSIsContainer -and $_.Name -ne "Documentatie" -and $_.Name -ne "WebshopFrontend" })) {
     foreach($item in $(Get-ChildItem -Path "$($folder.FullName)\src")) {
         if($item.Name -like "*.Test" ) {
-            dotnet test "$($item.FullName)" -xml TEST-TestBuild.xml 
+            dotnet test "$($item.FullName)" -xml $($item.Name)-TestBuild.xml --no-build
         }
     }            
 }    
