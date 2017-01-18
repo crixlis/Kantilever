80d124b5af150755ce82dbb42880a2d5d1480bd2
Nick Knaake <>
2017-01-18 11:12:11 +0100

    Updated Run-Tests.ps1

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -2,7 +2,7 @@ param(
     $rootURL
 )
 
-foreach ($folder in $(Get-Content -Path $rootURL)) {
+foreach ($folder in $(Get-ChildItem -Path $rootURL)) {
     if ($folder.Name -like "*.Test") {
         dotnet test -xml TEST-TestBuild.xml
     }
