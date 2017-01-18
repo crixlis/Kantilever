748f302059f2672ada554424bbe250197e84dfd0
pmeijden <patrickvdmeijden@live.nl>
2017-01-18 10:18:09 +0100

    WebshopContextFactory toegevoegd

---
+++ WebshopAPI/src/Webshop.Database/WebshopContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webshop.Database
+{
+    public class WebshopContextFactory : IDbContextFactory<WebshopContext>
+    {
+        public WebshopContext Create(DbContextFactoryOptions options)
+        {
+            var builder = new DbContextOptionsBuilder<WebshopContext>()
+            .UseSqlServer(@"Server=.\SQLEXPRESS;Database=Afspraken;Trusted_Connection=true");
+
+            return new WebshopContext(builder.Options);
+        }
+    }
+}
