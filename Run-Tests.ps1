658a7391c6b4a6cdcc54f5b0affac92a8a6c08e9
n.knaake@gmail.com <n.knaake@gmail.com>
2017-01-18 11:43:19 +0100

    edit on Run-Tests

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -2,8 +2,11 @@ param(
     $rootURL
 )
 
-foreach ($folder in $(Get-Content -Path $rootURL)) {
-    if ($folder.Name -like "*.Test") {
-        dotnet test -xml TEST-TestBuild.xml
-    }
-}
+foreach ($folder in $(Get-ChildItem -Path $rootURL | Where-Object{ $_.PSIsContainer -and $_.Name -ne "Documentatie" -and $_.Name -ne "WebshopFrontend" })) {
+    foreach($item in $(Get-ChildItem -Path "$($folder.FullName)\src")) {
+        if($item.Name -like "*.Test" ) {
+            dotnet test "$($item.FullName)" -xml TEST-TestBuild.xml 
+        }
+    }            
+}    
+
