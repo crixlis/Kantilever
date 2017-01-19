a4066f2a20db94f6c30b08ce5de4b24c67464c83
Gino Bos <GinoB@CURSISTM04>
2017-01-19 12:19:21 +0100

    Smoketest op controller

--- WebshopBeheer/src/WebshopBeheer.Test/CommercieelManagerControllerTests.cs
+++ WebshopBeheer/src/WebshopBeheer.Test/CommercieelManagerControllerTests.cs
@@ -1,9 +1,21 @@
-﻿using WebshopBeheer.Controllers;
+﻿using System.Net;
+using System.Net.Http;
 using Xunit;
 
 namespace WebshopBeheer.Test
 {
     public class CommercieelManagerControllerTests
     {
+        [Fact]
+        public async void TestOfPaginaBereikbaarIsNaHetStarten()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetAsync("Http://localhost:15282/CommercieelManager/Index");
+                var statuscode = result.StatusCode;
+
+                Assert.Equal(HttpStatusCode.OK, statuscode);
+            }
+        }
     }
 }
