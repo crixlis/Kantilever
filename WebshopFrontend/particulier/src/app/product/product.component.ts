20a936d4b2d6fde6ca681a94e70155173274e94c
klaasteake <klaasteake@hotmail.com>
2017-01-19 12:06:04 +0100

    Product pagina relevante informatie afgestemd met PO

--- WebshopFrontend/particulier/src/app/product/product.component.ts
+++ WebshopFrontend/particulier/src/app/product/product.component.ts
@@ -22,6 +22,7 @@ export class ProductComponent implements OnInit {
   public productLeverancierCode: string;
   public productLeverancier: string;
   public productCatagorieen: string[];
+  public productVoorraad: number;
 
   constructor(private route: ActivatedRoute, private shoppingCart : shoppingCartService) {
   }
@@ -30,16 +31,19 @@ export class ProductComponent implements OnInit {
   ngOnInit() {
      this._sub = this.route.params.subscribe(params => {
        this.productId = +params['id']; // (+) converts string 'id' to a number
+
+       //Data ophalen, nu eerst mock data tot we een GET request hebben
+      this.productTitel = 'Altec Manta - Stadsfiets - Mannen - Zwart - 61 cm';
+      this.productOmschrijving = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quo, dolor ut eius accusantium repellat consequatur, dignissimos error in adipisci, sit placeat minima, harum dicta nam magnam expedita obcaecati. Iste veritatis adipisci tempore voluptatum, sit quibusdam, natus reiciendis repellendus tempora! Quam temporibus velit ullam nisi recusandae, asperiores mollitia voluptatem quo.';
+      this.productPrijs = 12345.40;
+      this.productLeverancier = 'Altec Manta';
+      this.productCatagorieen = ['Fietsen', 'Stadsfietsen'];
+      this.productLeverbaarTot = '20-02-2017';
+      this.productVoorraad = 6;
     });
 
     //
-    //Data ophalen, nu eerst mock data tot we een GET request hebben
-    this.productTitel = 'Altec Manta - Stadsfiets - Mannen - Zwart - 61 cm';
-    this.productOmschrijving = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quo, dolor ut eius accusantium repellat consequatur, dignissimos error in adipisci, sit placeat minima, harum dicta nam magnam expedita obcaecati. Iste veritatis adipisci tempore voluptatum, sit quibusdam, natus reiciendis repellendus tempora! Quam temporibus velit ullam nisi recusandae, asperiores mollitia voluptatem quo.';
-    this.productPrijs = 12345.40;
-    this.productLeverancier = 'Altec Manta';
-    this.productCatagorieen = ['Fietsen', 'Stadsfietsen'];
-    this.productLeverbaarTot = '20-02-2017';
+    
   }
 
   ngOnDestroy() {
