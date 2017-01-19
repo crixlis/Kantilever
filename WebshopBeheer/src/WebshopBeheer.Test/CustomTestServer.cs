e9cf8b371d2ad815ef5aeaeaa020e76bfdf5351f
Gino Bos <GinoB@CURSISTM04>
2017-01-19 14:18:17 +0100

    CustomTestServer als static class

---
+++ WebshopBeheer/src/WebshopBeheer.Test/CustomTestServer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.IO;
+
+namespace WebshopBeheer.Test
+{
+    public static class CustomTestServer
+    {
+        public static TestServer Start()
+        {
+            return new TestServer(new WebHostBuilder()
+                            .UseStartup<Startup>()
+                            .UseContentRoot(Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "src", "WebshopBeheer")));
+        }
+    }
+}
