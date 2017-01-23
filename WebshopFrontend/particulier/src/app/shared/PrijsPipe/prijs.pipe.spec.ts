483a3dd7bccfbdc2b225a1ef078ceee54ff61819
Jeroen Duel <JeroenD@CURSISTM07>
2017-01-23 15:10:27 +0100

    Pricepipe tests

--- WebshopFrontend/particulier/src/app/shared/PrijsPipe/prijs.pipe.spec.ts
+++ WebshopFrontend/particulier/src/app/shared/PrijsPipe/prijs.pipe.spec.ts
@@ -9,8 +9,23 @@ describe('PrijsPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('xxx', () => {
+  it('price pipe should display 12 euro as \'€12\'', () => {
     let pipe = new PrijsPipe();
     expect(pipe.transform(12.00)).toBe('€12');
+  });
+
+  it('price pipe should display 12.99 euro as \'€12,99\'', () => {
+    let pipe = new PrijsPipe();
+    expect(pipe.transform(12.99)).toBe('€12,99');
+  });
+
+  it('price pipe should display 12000.00 euro as \'€12.000\'', () => {
+    let pipe = new PrijsPipe();
+    expect(pipe.transform(12000.00)).toBe('€12.000');
+  });
+
+  it('price pipe should display 12999.99 euro as \'€12.999,99\'', () => {
+    let pipe = new PrijsPipe();
+    expect(pipe.transform(12999.99)).toBe('€12.999,99');
   });
 });
