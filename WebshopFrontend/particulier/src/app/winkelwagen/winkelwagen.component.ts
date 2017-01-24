14825829d5e43c40eddcc2f97220b601b9286bc6
Gino Bos <GinoB@CURSISTM04>
2017-01-24 12:40:57 +0100

    Winkelwagentje werkt nu! Let op: wel nog steeds statische dat in api controller!

--- WebshopFrontend/particulier/src/app/winkelwagen/winkelwagen.component.ts
+++ WebshopFrontend/particulier/src/app/winkelwagen/winkelwagen.component.ts
@@ -34,4 +34,8 @@ export class WinkelwagenComponent implements OnInit {
         });
     });
   }
+
+  public goToProductPage(productId : number) {
+    window.location.href = 'product/' + productId;
+  }
 }
