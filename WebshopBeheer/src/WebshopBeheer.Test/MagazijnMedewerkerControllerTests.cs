5fe6471654c0b14d9a3e3287db702d49f1a52acc
klaasteake <klaasteake@hotmail.com>
2017-01-24 10:34:57 +0100

    MagazijnMedewerkerControllerLaatAlleenBestellingenZienDieNogNietGekeurdZijn

--- WebshopBeheer/src/WebshopBeheer.Test/MagazijnMedewerkerControllerTests.cs
+++ WebshopBeheer/src/WebshopBeheer.Test/MagazijnMedewerkerControllerTests.cs
@@ -115,15 +115,51 @@ namespace WebshopBeheer.Test
                 }
             };
 
+            var nognietgekeurdebestelling2 = new Bestelling()
+            {
+                Id = 4,
+                Status = 0,
+                Klant = new Klant() { Id = 10, Achternaam = "Slager" },
+                Artikelen = new List<Artikel>()
+                {
+                    new Artikel() { Id = 5, Beschrijving= "Stuur", Voorraad = 6 },
+                    new Artikel() { Id = 7, Beschrijving= "Fietscomputer", Voorraad = 2 }
+                }
+            };
+
+            var gekeurdebestelling = new Bestelling()
+            {
+                Id = 2,
+                Status = 1,
+                Klant = new Klant() { Id = 10, Achternaam = "Slager" },
+                Artikelen = new List<Artikel>()
+                {
+                    new Artikel() { Id = 8, Beschrijving= "Zadel", Voorraad = 6 },
+                    new Artikel() { Id = 9, Beschrijving= "Ballenknijper met zeemleer", Voorraad = 2 }
+                }
+            };
+            var afgekeurdebestelling = new Bestelling()
+            {
+                Id = 3,
+                Status = 2,
+                Klant = new Klant() { Id = 10, Achternaam = "Slager" },
+                Artikelen = new List<Artikel>()
+                {
+                    new Artikel() { Id = 10, Beschrijving= "Fietsbel", Voorraad = 6 },
+                    new Artikel() { Id = 11, Beschrijving= "Stuur", Voorraad = 2 }
+                }
+            };
+
             var options = new DbContextOptionsBuilder<WebshopBeheerContext>()
-               .UseInMemoryDatabase(databaseName: "MagazijnMedewerkerTest4")
+               .UseInMemoryDatabase(databaseName: "MagazijnMedewerkerTest5")
                .Options;
 
             using (var context = new WebshopBeheerContext(options))
             {
 
-
+                context.Bestellingen.Add(gekeurdebestelling);
                 context.Bestellingen.Add(nognietgekeurdebestelling);
+                context.Bestellingen.Add(afgekeurdebestelling);
                 context.SaveChanges();
 
                 var controller = new MagazijnMedewerkerController(context);
@@ -135,8 +171,15 @@ namespace WebshopBeheer.Test
                 var result = Assert.IsType<ViewResult>(view);
                 var model = Assert.IsAssignableFrom<IEnumerable<Bestelling>>(result.Model);
 
-                Assert.True(model.Any());
+                Assert.True(model.Count() == 1);
+                Assert.True(model.First() == nognietgekeurdebestelling);
             }
+        }
+
+        [Fact]
+        public void ddd()
+        {
+
         }
     }
 }
