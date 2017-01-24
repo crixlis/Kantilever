7e82d2263f3d4994525145aeb07f3de37fbe7ca4
n.knaake@gmail.com <n.knaake@gmail.com>
2017-01-24 09:46:50 +0100

    added db context to the the program.cs

---
+++ FactuurService/src/FactuurService.Database/Migrations/20170124083436_initial.Designer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using FactuurService.Database;
+
+namespace FactuurService.Database.Migrations
+{
+    [DbContext(typeof(FactuurServiceContext))]
+    [Migration("20170124083436_initial")]
+    partial class initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
+
+            modelBuilder.Entity("FactuurService.Database.Artikel", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("FactuurId");
+
+                    b.Property<string>("Naam");
+
+                    b.Property<decimal>("Prijs");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("FactuurId");
+
+                    b.ToTable("Artikel");
+                });
+
+            modelBuilder.Entity("FactuurService.Database.Factuur", b =>
+                {
+                    b.Property<int>("Id");
+
+                    b.Property<DateTime>("BetalenVoorDatum");
+
+                    b.Property<int?>("KlantId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("KlantId");
+
+                    b.ToTable("Facturen");
+                });
+
+            modelBuilder.Entity("FactuurService.Database.Klant", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Achternaam");
+
+                    b.Property<string>("Adres");
+
+                    b.Property<string>("Plaatsnaam");
+
+                    b.Property<string>("Postcode");
+
+                    b.Property<int>("Telefoonnummer");
+
+                    b.Property<string>("Voornaam");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Klant");
+                });
+
+            modelBuilder.Entity("FactuurService.Database.Artikel", b =>
+                {
+                    b.HasOne("FactuurService.Database.Factuur")
+                        .WithMany("Artikelen")
+                        .HasForeignKey("FactuurId");
+                });
+
+            modelBuilder.Entity("FactuurService.Database.Factuur", b =>
+                {
+                    b.HasOne("FactuurService.Database.Klant", "Klant")
+                        .WithMany()
+                        .HasForeignKey("KlantId");
+                });
+        }
+    }
+}
