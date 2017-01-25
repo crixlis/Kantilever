0f920b8067fa58e6e6bc81478e51caebced9b171
Gino Bos <GinoB@CURSISTM04>
2017-01-25 11:44:43 +0100

    Form tot nu toe...

--- WebshopFrontend/particulier/src/app/bestellingAfronden/bestellingAfronden.component.ts
+++ WebshopFrontend/particulier/src/app/bestellingAfronden/bestellingAfronden.component.ts
@@ -1,31 +1,33 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { ShoppingCartService, ArtikelService, Artikel, PrijsPipe } from './../shared';
-/*import { FormGroup, Validators, FormBuilder } from '@angular/forms';*/
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Http } from '@angular/http';
+import { ShoppingCartService, ArtikelService, Artikel, PrijsPipe } from './../shared';
 
 @Component({
+  moduleId: module.id,
   selector: 'bestellingAfronden',
+  providers: [],
   templateUrl: './bestellingAfronden.component.html',
   styleUrls: ['./bestellingAfronden.component.css']
 })
 export class BestellingAfrondenComponent implements OnInit {
 
-  constructor(private _shoppingCart : ShoppingCartService, private _artikelService : ArtikelService
-  /*, private http: Http,
-		private fb: FormBuilder*/
-    ) {
-  }
+  constructor(
+    private _shoppingCart : ShoppingCartService, 
+    private _artikelService : ArtikelService,
+    private http: Http,
+		private fb: FormBuilder) {}
 
   artikelen : Artikel[] = [];
   error : string;
-  /*bestellingAfrondenForm : FormGroup;*/
+  bestellingAfrondenForm : FormGroup;
 
   @Output() onNewAmountProducts = new EventEmitter();
 
     ngOnInit() {
-       /*this.bestellingAfrondenForm = this.fb.group({
+       this.bestellingAfrondenForm = this.fb.group({
 
-      });*/
+      });
     }
 
     bestellingAfronden(){
